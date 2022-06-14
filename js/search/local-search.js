3eaf14f311c004a52c52ba967e73df7ebc245bf5
gonisan <1659632295@qq.com>
2022-06-14 19:01:10 +0800

    Site updated: 2022-06-14 19:01:10

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -1,2 +1,188 @@
-var y=(i,r,o)=>{let n=o[0],{position:s,word:e}=n,l=[],f=new Set;for(;s+e.length<=r&&o.length!==0;){f.add(e),l.push({position:s,length:e.length});let c=s+e.length;for(o.shift();o.length!==0&&(n=o[0],s=n.position,e=n.word,c>s);)o.shift()}return{hits:l,start:i,end:r,count:f.size}},L=(i,r,o=!1)=>{let n=[],s=new Set;return i.forEach(e=>{let l=e.length;if(l===0)return;let f=0,c=-1;for(o||(r=r.toLowerCase(),e=e.toLowerCase());(c=r.indexOf(e,f))>-1;)n.push({position:c,word:e}),s.add(e),f=c+l}),n.sort((e,l)=>e.position!==l.position?e.position-l.position:l.word.length-e.word.length),[n,s]},S=(i,r)=>{let o="",n=r.start;for(let{position:s,length:e}of r.hits)o+=i.substring(n,s),n=s+e,o+=`<mark class="search-keyword">${i.substr(s,e)}</mark>`;return o+=i.substring(n,r.end),o},b=(i,r,o)=>{if(!i){console.warn("`hexo-generator-searchdb` plugin is not installed!");return}let n=[],s=document.getElementById(r),e=document.getElementById(o),l=c=>{let p=[];return n.forEach(({title:t,content:m,url:h})=>{let[C,a]=L(c,t),[u,E]=L(c,m),x=new Set([...a,...E]).size,$=C.length+u.length;if($===0)return;let I=[];C.length!==0&&I.push(y(0,t.length,C));let O=[];for(;u.length!==0;){let d=u[0],{position:g}=d,M=Math.max(0,g-20),T=Math.min(m.length,g+80);O.push(y(M,T,u))}O.sort((d,g)=>d.count!==g.count?g.count-d.count:d.hits.length!==g.hits.length?g.hits.length-d.hits.length:d.start-g.start);let w="";h=new URL(h,location.origin),h.searchParams.append("highlight",c.join(" ")),I.length!==0?w+=`<li><a href="${h.href}" class="search-result-title">${S(t,I[0])}</a>`:w+=`<li><a href="${h.href}" class="search-result-title">${t}</a>`,O.forEach(d=>{w+=`<a href="${h.href}"><p class="search-result">${S(m,d)}...</p></a>`}),w+="</li>",p.push({item:w,id:p.length,hitCount:$,includedCount:x})}),p};(()=>{let c=!i.endsWith("json");fetch(i).then(p=>p.text()).then(p=>{n=c?[...new DOMParser().parseFromString(p,"text/xml").querySelectorAll("entry")].map(t=>({title:t.querySelector("title").textContent,content:t.querySelector("content").textContent,url:t.querySelector("url").textContent})):JSON.parse(p),n=n.filter(t=>t.title).map(t=>(t.title=t.title.trim(),t.content=t.content?t.content.trim().replace(/<[^>]+>/g,""):"",t.url=decodeURIComponent(t.url).replace(/\/{2,}/g,"/"),t)),s&&s.addEventListener("input",()=>{let t=s.value.trim().toLowerCase(),m=t.split(/[-\s]+/),h=[];t.length>0&&(h=l(m)),h.sort((a,u)=>a.includedCount!==u.includedCount?u.includedCount-a.includedCount:a.hitCount!==u.hitCount?u.hitCount-a.hitCount:u.id-a.id);let C=window.CONFIG.i18n.hits.replace(/\$\{hits}/,h.length);e.innerHTML=`<div class="search-stats">${C}</div>
-            <ul class="search-result-list">${h.map(a=>a.item).join("")}</ul>`})})})()};document.addEventListener("DOMContentLoaded",()=>{b(window.CONFIG.local_search.path,"local-search-input","local-search-result")});
+window.addEventListener('load', () => {
+  let loadFlag = false
+  let dataObj = []
+  const $searchMask = document.getElementById('search-mask')
+
+  const openSearch = () => {
+    const bodyStyle = document.body.style
+    bodyStyle.width = '100%'
+    bodyStyle.overflow = 'hidden'
+    btf.animateIn($searchMask, 'to_show 0.5s')
+    btf.animateIn(document.querySelector('#local-search .search-dialog'), 'titleScale 0.5s')
+    setTimeout(() => { document.querySelector('#local-search-input input').focus() }, 100)
+    if (!loadFlag) {
+      search()
+      loadFlag = true
+    }
+    // shortcut: ESC
+    document.addEventListener('keydown', function f (event) {
+      if (event.code === 'Escape') {
+        closeSearch()
+        document.removeEventListener('keydown', f)
+      }
+    })
+  }
+
+  const closeSearch = () => {
+    const bodyStyle = document.body.style
+    bodyStyle.width = ''
+    bodyStyle.overflow = ''
+    btf.animateOut(document.querySelector('#local-search .search-dialog'), 'search_close .5s')
+    btf.animateOut($searchMask, 'to_hide 0.5s')
+  }
+
+  const searchClickFn = () => {
+    document.querySelector('#search-button > .search').addEventListener('click', openSearch)
+  }
+
+  const searchClickFnOnce = () => {
+    document.querySelector('#local-search .search-close-button').addEventListener('click', closeSearch)
+    $searchMask.addEventListener('click', closeSearch)
+    if (GLOBAL_CONFIG.localSearch.preload) dataObj = fetchData(GLOBAL_CONFIG.localSearch.path)
+  }
+
+  // check url is json or not
+  const isJson = url => {
+    const reg = /\.json$/
+    return reg.test(url)
+  }
+
+  const fetchData = async (path) => {
+    let data = []
+    const response = await fetch(path)
+    if (isJson(path)) {
+      data = await response.json()
+    } else {
+      const res = await response.text()
+      const t = await new window.DOMParser().parseFromString(res, 'text/xml')
+      const a = await t
+      data = [...a.querySelectorAll('entry')].map(item =>{
+        return {
+          title: item.querySelector('title').textContent,
+          content: item.querySelector('content') && item.querySelector('content').textContent,
+          url: item.querySelector('url').textContent
+        }
+      })
+    }
+    if (response.ok) {
+      const $loadDataItem = document.getElementById('loading-database')
+      $loadDataItem.nextElementSibling.style.display = 'block'
+      $loadDataItem.remove()
+    }
+    return data
+  }
+
+  const search = () => {
+    if (!GLOBAL_CONFIG.localSearch.preload) {
+      dataObj = fetchData(GLOBAL_CONFIG.localSearch.path)
+    }
+
+    const $input = document.querySelector('#local-search-input input')
+    const $resultContent = document.getElementById('local-search-results')
+    const $loadingStatus = document.getElementById('loading-status')
+
+    $input.addEventListener('input', function () {
+      const keywords = this.value.trim().toLowerCase().split(/[\s]+/)
+      if (keywords[0] !== '') $loadingStatus.innerHTML = '<i class="fas fa-spinner fa-pulse"></i>'
+
+      $resultContent.innerHTML = ''
+      let str = '<div class="search-result-list">'
+      if (keywords.length <= 0) return
+      let count = 0
+      // perform local searching
+      dataObj.then(data => {
+        data.forEach(data => {
+          let isMatch = true
+          let dataTitle = data.title ? data.title.trim().toLowerCase() : ''
+          const dataContent = data.content ? data.content.trim().replace(/<[^>]+>/g, '').toLowerCase() : ''
+          const dataUrl = data.url.startsWith('/') ? data.url : GLOBAL_CONFIG.root + data.url
+          let indexTitle = -1
+          let indexContent = -1
+          let firstOccur = -1
+          // only match articles with not empty titles and contents
+          if (dataTitle !== '' || dataContent !== '') {
+            keywords.forEach((keyword, i) => {
+              indexTitle = dataTitle.indexOf(keyword)
+              indexContent = dataContent.indexOf(keyword)
+              if (indexTitle < 0 && indexContent < 0) {
+                isMatch = false
+              } else {
+                if (indexContent < 0) {
+                  indexContent = 0
+                }
+                if (i === 0) {
+                  firstOccur = indexContent
+                }
+              }
+            })
+          } else {
+            isMatch = false
+          }
+
+          // show search results
+          if (isMatch) {
+            if (firstOccur >= 0) {
+              // cut out 130 characters
+              // let start = firstOccur - 30 < 0 ? 0 : firstOccur - 30
+              // let end = firstOccur + 50 > dataContent.length ? dataContent.length : firstOccur + 50
+              let start = firstOccur - 30
+              let end = firstOccur + 100
+              let pre = ''
+              let post = ''
+
+              if (start < 0) {
+                start = 0
+              }
+
+              if (start === 0) {
+                end = 100
+              } else {
+                pre = '...'
+              }
+
+              if (end > dataContent.length) {
+                end = dataContent.length
+              } else {
+                post = '...'
+              }
+
+              let matchContent = dataContent.substring(start, end)
+
+              // highlight all keywords
+              keywords.forEach(keyword => {
+                const regS = new RegExp(keyword, 'gi')
+                matchContent = matchContent.replace(regS, '<span class="search-keyword">' + keyword + '</span>')
+                dataTitle = dataTitle.replace(regS, '<span class="search-keyword">' + keyword + '</span>')
+              })
+
+              str += '<div class="local-search__hit-item"><a href="' + dataUrl + '" class="search-result-title">' + dataTitle + '</a>'
+              count += 1
+
+              if (dataContent !== '') {
+                str += '<p class="search-result">' + pre + matchContent + post + '</p>'
+              }
+            }
+            str += '</div>'
+          }
+        })
+        if (count === 0) {
+          str += '<div id="local-search__hits-empty">' + GLOBAL_CONFIG.localSearch.languages.hits_empty.replace(/\$\{query}/, this.value.trim()) +
+            '</div>'
+        }
+        str += '</div>'
+        $resultContent.innerHTML = str
+        if (keywords[0] !== '') $loadingStatus.innerHTML = ''
+        window.pjax && window.pjax.refresh($resultContent)
+      })
+    })
+  }
+
+  searchClickFn()
+  searchClickFnOnce()
+
+  // pjax
+  window.addEventListener('pjax:complete', () => {
+    !btf.isHidden($searchMask) && closeSearch()
+    searchClickFn()
+  })
+})
